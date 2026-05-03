31f386de7f9008b49ac43766aff1b64c5bda2fd2
anucacorne <anucacornea520@gmail.com>
2026-05-03 16:40:41 +0300

    Fix CORS and update backend URL

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,8 @@ const errorHandler = require('./middleware/errorHandler');
 const app  = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
+
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend')));
